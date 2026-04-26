38916ddaf99893b586b76404af26809254414563
Jason Lee <jason.lee@leeminsoft.com>
2026-04-27 00:01:43 +0900

    Use gcloud SSH key for VM deploy

--- scripts/deploy-vm.sh
+++ scripts/deploy-vm.sh
@@ -8,8 +8,14 @@ BRANCH="${BRANCH:-main}"
 WEB_ORIGIN="${WEB_ORIGIN:-https://gamezip.kr}"
 APP_PORT="${APP_PORT:-3001}"
 CERTBOT_EMAIL="${CERTBOT_EMAIL:-}"
+SSH_KEY="${SSH_KEY:-${HOME}/.ssh/google_compute_engine}"
 
-ssh "${TARGET}" "mkdir -p /tmp/web-game-deploy"
-scp scripts/bootstrap-vm.sh "${TARGET}:/tmp/web-game-deploy/bootstrap-vm.sh"
-ssh "${TARGET}" \
+SSH_ARGS=()
+if [ -f "${SSH_KEY}" ]; then
+  SSH_ARGS=(-i "${SSH_KEY}" -o IdentitiesOnly=yes)
+fi
+
+ssh "${SSH_ARGS[@]}" "${TARGET}" "mkdir -p /tmp/web-game-deploy"
+scp "${SSH_ARGS[@]}" scripts/bootstrap-vm.sh "${TARGET}:/tmp/web-game-deploy/bootstrap-vm.sh"
+ssh "${SSH_ARGS[@]}" "${TARGET}" \
   "APP_DIR='${APP_DIR}' REPO_URL='${REPO_URL}' BRANCH='${BRANCH}' WEB_ORIGIN='${WEB_ORIGIN}' APP_PORT='${APP_PORT}' CERTBOT_EMAIL='${CERTBOT_EMAIL}' bash /tmp/web-game-deploy/bootstrap-vm.sh"
